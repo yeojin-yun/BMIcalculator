ec7b5ef5a90167b1ffd9ab574497c467562163ae
순진이 <sail041@naver.com>
2022-02-09 22:28:01 +0900

    Start BMIcalculator

---
+++ BMIcalculator/BMIcalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMIcalculator
+//
+//  Created by 순진이 on 2022/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
